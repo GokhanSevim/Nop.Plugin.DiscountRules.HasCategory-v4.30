da39cc1c2cc2cee55166f868d8af453d304d4af8
Gökhan SEVİM <mail@gokhansevim.com>
2022-01-03 11:38:09 +0300

    Initial commit

---
+++ Models/CartModel.cs
@@ -0,0 +1,15 @@
+﻿using Nop.Core.Domain.Catalog;
+using Nop.Core.Domain.Orders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nop.Plugin.DiscountRules.HasCategory.Models
+{
+    public class CartModel
+    {
+        public ShoppingCartItem CartItem { get; set; }
+
+        public IList<ProductCategory> Category { get; set; }
+    }
+}
---
+++ Validators/RequirementModelValidator.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.RegularExpressions;
+using FluentValidation;
+using Nop.Plugin.DiscountRules.HasCategory.Models;
+using Nop.Services.Localization;
+using Nop.Web.Framework.Validators;
+
+namespace Nop.Plugin.DiscountRules.HasCategory.Validators
+{
+    /// <summary>
+    /// Represents an <see cref="RequirementModel"/> validator.
+    /// </summary>
+    public class RequirementModelValidator : BaseNopValidator<RequirementModel>
+    {
+        public RequirementModelValidator(ILocalizationService localizationService)
+        {
+            RuleFor(model => model.DiscountId)
+                .NotEmpty()
+                .WithMessage(localizationService.GetResource("Plugins.DiscountRules.HasCategory.Fields.DiscountId.Required"));
+            RuleFor(model => model.CategoryIds)
+                .NotEmpty()
+                .WithMessage(localizationService.GetResource("Plugins.DiscountRules.HasCategory.Fields.CategoryIds.Required"));
+            RuleFor(model => model.CategoryIds)
+                .Must(value => !Regex.IsMatch(value, @"(?!\d+)(?:[^ ,:-])"))
+                .WithMessage(localizationService.GetResource("Plugins.DiscountRules.HasCategory.Fields.CategoryIds.InvalidFormat"))
+                .When(model => !string.IsNullOrWhiteSpace(model.CategoryIds));
+        }
+    }
+}
